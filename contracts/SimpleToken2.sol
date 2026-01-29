1c6c7debf54b9468776c73cfacbf151f78c73b33
yuly <2765352250@163.com>
2026-01-29 13:26:05 +0800

    '测试某合约调用实现IERC20代币合约中的授权转账函数msg.sender值是谁的问题'

--- contracts/SimpleToken2.sol
+++ contracts/SimpleToken2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 /**
  * 1.部署SimpleToken合约
  * 2.复制SimpleToken合约地址
- * 3.部署TokenSwap合约同时填入复制的SimpleToken合约地址，界面上VALUE框中，选择Ether，输入10个。
+ * 3.部署TokenSwap合约同时填入复制的SimpleToken合约地址
  * 4.调用SimpleToken合约的 approve 函数给 TokenSwap 合约地址授权"1000000000000000000"个。
  * 5.调用TokenSwap合约的swap()函数，输入"500000000000000000"个。
  * 6.查看日志decoded output：
@@ -41,6 +41,5 @@ contract TokenSwap {
         return (msg.sender, simpleToken.transferFrom(msg.sender, address(this), amount));
         // simpleToken.transferFrom()里面的代码中msg.sender是谁，是本合约TokenSwap地址，还是钱包账户地址。
         // 答：return (钱包账户地址，本合约TokenSwap地址);
-        // 本合约账户中必须有足够的ETH来支付调用 simpleToken.transferFrom() 函数的gas费。
     }
 }
