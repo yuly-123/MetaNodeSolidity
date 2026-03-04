79878fd0f7dfe11fa9429f082e428b5da40e2556
yuly <2765352250@163.com>
2026-03-04 17:31:29 +0800

    '临时提交'

--- contracts/ContractUpgrade/Upgrade.sol
+++ contracts/ContractUpgrade/Upgrade.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 
+// 选择器冲突的例子
+// 去掉注释后，合约不会通过编译，因为两个函数有着相同的选择器
+contract Foo {
+    bytes4 public selector1 = bytes4(keccak256("burn(uint256)"));
+    bytes4 public selector2 = bytes4(keccak256("collate_propagate_storage(bytes16)"));
+    // function burn(uint256) external {}
+    // function collate_propagate_storage(bytes16) external {}
+}
+
 /**
  * Upgrade简单的可升级合约，管理员可以通过升级函数更改逻辑合约地址，从而改变合约的逻辑。
  */
@@ -18,13 +27,15 @@ contract Upgrade {
 
     }
     // fallback函数，将调用委托给逻辑合约
+    // 不能被admin调用，避免选择器冲突引发意外
     fallback() external payable {
+        require(msg.sender != admin, "caller not allowed of admin");
         (bool success, bytes memory data) = implementation.delegatecall(msg.data);
     }
 
     // 升级函数，改变逻辑合约地址，只能由admin调用
     function upgrade(address newImplementation) external {
-        require(msg.sender == admin);
+        require(msg.sender == admin, "not admin");
         implementation = newImplementation;
     }
 }
--- contracts/TestTemp.sol
+++ contracts/TestTemp.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.19;
 
 contract TestTemp {
+    bytes4 public selector1 = bytes4(keccak256("burn(uint256)"));
+    bytes4 public selector2 = bytes4(keccak256("collate_propagate_storage(bytes16)"));
     event Input(address indexed from, uint256 value);
+    
     function show() public  {
         emit Input(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 213);
     }
