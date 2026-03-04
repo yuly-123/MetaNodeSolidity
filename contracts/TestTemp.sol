76d7effd884f3813f632145a2414d422e421754b
yuly <2765352250@163.com>
2026-03-04 09:17:10 +0800

    '测试代码'

--- contracts/TestTemp.sol
+++ contracts/TestTemp.sol
@@ -2,5 +2,8 @@
 pragma solidity ^0.8.19;
 
 contract TestTemp {
-    
+    event Input(address indexed from, uint256 value);
+    function show() public  {
+        emit Input(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 213);
+    }
 }
