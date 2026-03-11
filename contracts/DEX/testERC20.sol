d2a3509f0c57db2c14c80b7eb3c0c482a65713b8
yuly <2765352250@163.com>
2026-03-11 19:41:13 +0800

    'DEX去中心化交易所'

---
+++ contracts/DEX/testERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract testERC20 is ERC20
+{
+    constructor(string memory name, string memory symbol) ERC20(name, symbol){
+        _mint(_msgSender(), 1000);
+    }
+}
